8ced17ffd070e00338466c9ef2c74fc92738bbf7
Aliaksandr Yerakhavets <aliaksandr_yerakhavets@epam.com>
2022-12-20 02:15:36 +0400

    feat: implement task 1

---
+++ src/task_1.1.ts
@@ -0,0 +1,18 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const reverseText = (text: String): String => text.split("").reverse().join("");
+
+const rl = readline.createInterface({ input, output });
+
+async function reverseInput() {
+  try {
+    const answer = await rl.question("Type a string to reverse: ");
+    console.log(`Reversed string: ${reverseText(answer)}`);
+    reverseInput();
+  } catch (_) {
+    rl.close();
+  }
+}
+
+(async () => await reverseInput())();
