123a78fbc53e3880bd029abf2cacc8b2825f3929
Aliaksandr Yerakhavets <aliaksandr_yerakhavets@epam.com>
2023-01-28 22:14:47 +0400

    chore: remove redundnat files

--- src/user_service/constants.ts
+++
@@ -1,6 +0,0 @@
-export const USER_NOT_FOUND = "User not found.";
-export const USER_DELETED = "User is removed.";
-export const USER_UPDATED = "User is updated.";
-export const ERROR_VALIDATING_PASSWORD = "Error validating the password";
-
-export const SERVER_PORT = 3000;
